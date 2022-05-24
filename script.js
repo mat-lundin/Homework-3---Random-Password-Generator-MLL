574b2bf95cfe1cee2d3f99e5d70458f89df5170d
Mat Lundin <matlundin.dev@gmail.com>
2022-05-24 18:49:51 -0500

    almost complete refactor of script, added checks to replace prompts

--- script.js
+++ script.js
@@ -1,212 +1,106 @@
-//initialize length as 0 for the while loop that controls the prompt
-var selects = {
-  length: 0,
-  upperCase: true,
-  lowerCase: true,
-  symbols: true,
-  numbers: true,
-};
-
-var fnArr = ['randomUpper','randomLower','randomSymb','randomNum'];
-
-var symbArr = ['!','@','#','$','%','^','&','*','(',')'];
-
-var pwd = '';
+// establish DOM variables
+const passDisp = document.getElementById('password')
+const genBtn = document.getElementById('generate');
+const copyBtn = document.getElementById('copy');
+const upperChk = document.getElementById('upper');
+const lowerChk = document.getElementById('lower');
+const symbChk = document.getElementById('symb');
+const lengthInp = document.getElementById('length');
 
 
-//random symbol
-function randomSymb() {
-  console.log('symbfn');
-  var symchar = symbArr[Math.floor(Math.random()*symbArr.length)];
-  return symchar;
-};
+// password length var
+let length = lengthInp.value;
 
-//random uppercase - idea from Traversy Media - https://www.youtube.com/watch?v=duNmhKgtcsI
+const charGen = [
+    {
+        fn: randomUpper,
+        select: true
+    },
+    {
+        fn: randomLower,
+        select: true
+    },
+    {
+        fn: randomSymb,
+        select: true
+    },
+];
+
+// functions to generate a random character of each type
+// uppercase is 65-90
 function randomUpper() {
-  console.log('upperfn');
-  var upperChar = String.fromCharCode(Math.floor(Math.random()*26)+65);
-  return upperChar;
-};
-
-//random lowercase - idea from Traversy Media - https://www.youtube.com/watch?v=duNmhKgtcsI
+    const charCode = Math.floor(Math.random() * 26 + 65);
+    return String.fromCharCode(charCode);
+}
+// lowercase is 97-122
 function randomLower() {
-  console.log('lowerfn');
-  var lowerChar = String.fromCharCode(Math.floor(Math.random()*26)+97);
-  return lowerChar;
-};
-
-//random number
-function randomNum() {
-  console.log('numfn');
-  var numChar = Math.floor(Math.random()*10).toString();
-  numChar = numChar;
-  console.log(typeof numChar);
-  return numChar;
+    const charCode = Math.floor(Math.random() * 26 + 97);
+    return String.fromCharCode(charCode);
 }
 
-//run function from function name as string
-function runFun(name) {
-  var pwdChar = ''
-  if (name === 'randomSymb') {
-    pwdChar = randomSymb();
-  } else if (name === 'randomUpper') {
-    pwdChar = randomUpper();
-  } else if (name === 'randomLower') {
-    pwdChar = randomLower();
-  } else if (name === 'randomNum') {
-    pwdChar = randomNum();
-  };
-  return pwdChar;
+// for now, symbols are 1-64
+// TODO: remove white space chars or just use a built in array of chars
+function randomSymb() {
+    const charCode = Math.floor(Math.random() * 64);
+    return String.fromCharCode(charCode);
 }
 
-
-
-//generate password based on criteria entered
-function generatePassword(len,upper,lower,symb,num) {
-  //reset pwd in case multiple passwords are generated
-  pwd = ''; 
-  if (upper && lower && symb && num) {
-    for (var i=0;i < len; i++) {
-      pwd += runFun(fnArr[Math.floor(Math.random()*fnArr.length)]); 
+// function to loop through and create the password
+// while loop to generate random number 1-3 while length has not been met
+// if selection for that index is false do not add anything, else execute function and push to array
+function passGen() {
+    let passArr = [];
+    if (!charGen[0].select && !charGen[1].select && !charGen[2].select) {
+        alert('Please select at least one type of character');
+        return;
+    } else {
+        while (passArr.length < length) {
+            const fnId = Math.floor(Math.random() * 3);
+            // console.log(passArr.length)
+            if (charGen[fnId].select) {
+                // console.log(true)
+                passArr.push(charGen[fnId].fn())
+            };
+        };
+        passDisp.innerHTML = passArr.join('');
     }
-  } else if (!upper && lower && symb && num) {
-    var fnMod = fnArr.slice(1);
-    console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-
-  } else if (!upper && !lower && symb && num) {
-      var fnMod = fnArr.slice(2);
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && !lower && !symb && num) {
-      var fnMod = fnArr.slice(3);
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && !lower && !symb && !num) {
-      var fnMod = fnArr.slice(0,1);
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && lower && !symb && !num) {
-      var fnMod = fnArr.slice(0,2);
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && lower && symb && !num) {
-      var fnMod = fnArr.slice(0,3);
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && !lower && symb && num) {
-      var fnMod = [fnArr[0],fnArr[2],fnArr[3]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && lower && !symb && num) {
-      var fnMod = [fnArr[0],fnArr[1],fnArr[3]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && !lower && !symb && num) {
-      var fnMod = [fnArr[0],fnArr[3]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && lower && !symb && num) {
-      var fnMod = [fnArr[1],fnArr[3]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && lower && symb && !num) {
-      var fnMod = [fnArr[1],fnArr[2]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (upper && !lower && symb && !num) {
-      var fnMod = [fnArr[0],fnArr[2]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && lower && !symb && !num) {
-      var fnMod = [fnArr[1]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && !lower && symb && !num) {
-      var fnMod = [fnArr[2]];
-      console.log(fnMod);
-      for (var i=0;i < len; i++) {
-        pwd += runFun(fnMod[Math.floor(Math.random()*fnMod.length)]);
-      }
-  } else if (!upper && !lower && !symb && !num) {
-      alert('You must select at least one character type.')
-      pwd = 'Click Generate Password to try again.'
-      }
-  return pwd;};
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-var copyBtn = document.querySelector("#copy")
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword(selects.length,selects.upperCase,selects.lowerCase,selects.symbols,selects.numbers);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  selects.length = 0;
-
 };
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-  while(selects.length < 8 || selects.length > 128 || isNaN(selects.length)) {
-    var len = prompt('Please enter a number of characters (8-128) for the password length.')
-    //add a specific alert if user adds non-numbers
-    if (len != null) {
-      selects.length = parseInt(len)
-      if (isNaN(selects.length)){
-      alert('Only numbers please!');
-      };
-    } 
-    else {
-      selects.length = 0;
-      return;
-    }
-  } {
-    copyBtn.innerText = 'Copy to Clipboard';
-    selects.upperCase = confirm('Include upper case letters? (OK = Yes, Cancel = No)');
-    selects.lowerCase = confirm('Include lower case letters? (OK = Yes, Cancel = No)');
-    selects.symbols = confirm('Include special characters/symbols? (OK = Yes, Cancel = No)');
-    selects.numbers = confirm('Include numbers? (OK = Yes, Cancel = No)');
-    writePassword();
-  }
+// event listeners for checkboxes
+
+// uppercase
+upperChk.addEventListener('click', () => {
+    charGen[0].select = upperChk.checked;
+});
+
+// lowercase
+lowerChk.addEventListener('click', () => {
+    charGen[1].select = lowerChk.checked;
+});
+
+// symbols
+symbChk.addEventListener('click', () => {
+    charGen[2].select = symbChk.checked;
+});
+
+// length
+lengthInp.addEventListener('change', () => {
+    const lengthVal = lengthInp.value;
+    if (lengthVal < 8 || lengthVal > 128) {
+        alert('Enter between 8 and 128 characters');
+        lengthInp.value = length;
+    } else {
+        length = lengthVal;
+    };
+
+})
+
+// event listeners on generate and copy buttons
+genBtn.addEventListener('click', () => {
+    passGen();
 
 });
 
-// Add event listener to copy button
-copyBtn.addEventListener('click', function(){
-  var pwd = document.querySelector("#password");
-  pwd.select();
-  document.execCommand("copy");
-  copyBtn.innerText = 'Copied!';
-  console.log(copyBtn, copyBtn.value);
-  
+copyBtn.addEventListener('click', () => {
+    console.log(charGen[0].select)
 });
